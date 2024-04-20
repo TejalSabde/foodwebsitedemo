5e08c4a95f59db5b20ea7f25b928a16abc8c136b
tejal <tej.hardikar@gmail.com>
2024-04-21 01:18:39 +0530

    CORS change

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,7 +262,8 @@ const form = document.forms['franch-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  fetch(scriptURL, { redirect: "follow", method: 'POST', body: new FormData(form), 
+  headers: { 'Access-Control-Allow-Origin': '*' ,"Content-Type": "application/json"}})
   .then(response => alert("Thank you! your form is submitted successfully." ))
   // .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
