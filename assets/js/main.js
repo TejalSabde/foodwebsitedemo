16444887bf5a57230b10252d295c8fe092108942
tejal <tej.hardikar@gmail.com>
2024-04-22 20:18:12 +0530

    form and growth story added

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,14 +256,13 @@
     });
   });
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycby1Wl0hnAFz4ZwW8hipqfCWdLXxCHVfJQhMcgjBoy1cjpURLobWxDh_3_wh65Xg7u3t/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxO8MC-yiRG5axZFjkvl7SkT0lLHM5alllMVcioDca5gUwBwtz-ePW2H_1R-iD3E0BO/exec'
 
 const form = document.forms['franch-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { redirect: "follow", method: 'POST', body: new FormData(form), 
-  headers: { 'Access-Control-Allow-Origin': '*' ,"Content-Type": "application/json"}})
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thank you! your form is submitted successfully." ))
   // .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
