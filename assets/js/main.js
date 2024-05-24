6e0231910901e8d0edb9642d68d153517e8ce984
tejal <tej.hardikar@gmail.com>
2024-05-24 14:15:39 +0530

    Auto-generated commit

---
+++ assets/js/main.js
@@ -0,0 +1,319 @@
+/**
+* Template Name: Shawarmaji
+* Template URL: https://bootstrapmade.com/Shawarmaji-free-bootstrap-html-template-corporate/
+* Updated: Mar 17 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Scroll to Franchise form
+   */
+  let franchiseFloatingButton = document.getElementById('franchise-floating-button');//select('.franchise-floating-button')
+  let franchiseForm = document.getElementById('franchiseForm');//select('.franchise-form')
+
+  if (franchiseFloatingButton) {
+    // const togglefranchiseForm = () => {
+    //   if (window.scrollY > 100) {
+    //     franchiseFloatingButton.classList.add('active')
+    //   } else {
+    //     franchiseFloatingButton.classList.remove('active')
+    //   }
+    // }
+    // window.addEventListener('load', togglefranchiseForm)
+    // onscroll(franchiseForm, togglefranchiseForm)
+
+    franchiseFloatingButton.addEventListener('click', function () {
+      // Check if the click is inside the franchiseForm
+      if (franchiseForm.contains(franchiseFloatingButton)) {
+        // Hide the link
+        franchiseFloatingButton.style.display = 'none';
+      }
+    });
+
+    franchiseForm.addEventListener('scroll', function () {
+      // Show the link if it's hidden and the scroll is not at the top
+      if (franchiseForm.scrollTop > 0 && franchiseLink.style.display === 'none') {
+        franchiseFloatingButton.style.display = '';
+      }
+    });
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.cta-btn-container .dropdown .cta-btn', function (e) {
+    if ((window.innerWidth <= 800)) {
+      // e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function (e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate  glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function (direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function (e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function () {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxO8MC-yiRG5axZFjkvl7SkT0lLHM5alllMVcioDca5gUwBwtz-ePW2H_1R-iD3E0BO/exec'
+
+  const form = document.forms['franch-form']
+  const loader = document.querySelector('.loading')
+  const loadingContainer = document.querySelector('.loading-active')
+  const successMsg = document.querySelector('.sent-message');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    loader.classList.add('d-block');
+    loadingContainer.classList.add('loadingContainer');
+    for (let [key, value] of new FormData(form)) {
+      console.log(`${key}: ${value}`)
+    }
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        loader.classList.remove('d-block');
+        loadingContainer.classList.remove('loadingContainer');
+        successMsg.classList.add('d-block')
+        setTimeout(() => {
+          successMsg.classList.remove('d-block')
+
+        }, 2000);
+        // alert("Thank you! your form is submitted successfully.")
+      })
+      // .then(() => { window.location.reload(); })
+      .catch(error => console.error('Error!', error.message))
+  })
+})()
